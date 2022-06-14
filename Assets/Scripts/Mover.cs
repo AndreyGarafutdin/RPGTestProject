6503c55826cb3ac30802765c7f4dbf84bc9eb2d2
Andrey Garafutdinov <>
2022-06-14 17:01:28 +0300

    Some little changes

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,10 +6,16 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-    
+
+    private Ray lastRay;
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100, Color.red, 10f );
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
